3f9d65323b3bd43a606918b0d7c24205a0f04765
adnanmughal1234 <adnanmughal03rgc@gmail.com>
2025-10-28 12:50:30 +0500

    resume update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
       <a
         className='flex justify-between items-center w-full text-gray-300'
-        href='https://drive.google.com/file/d/16UzVNKQTeD_-jVTQNKo30aH2ttMsSGme/view?usp=sharing'
+        href='https://drive.google.com/file/d/1w9HxRG6AYR81i3G_8xGf8PxjDHMxTtNI/view?usp=sharing'
         target='_blank'
         rel='noreferrer'
       >
